fa8970967ec7f9b2d7c760c1c9696216d9efe706
Emanuel Peire <emapeire@outlook.com>
2023-12-06 20:16:49 -0300

    Add content type header and update response messages

--- http.js
+++ http.js
@@ -4,10 +4,20 @@ const { findAvailablePort } = require('./free-port.js')
 const desiredPort = process.env.PORT ?? 3000
 
 const processRequest = (req, res) => {
+  res.setHeader('Content-Type', 'text/html; charset=utf-8')
+
   if (req.url === '/') {
+    console.log('Request received')
     res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('Welcome to the homepage!')
+    res.end('<h1>Welcome to the homepage! 🎉</h1>')
+  } else if (req.url === '/about') {
+    console.log('Request received')
+    res.statusCode = 200
+    res.end('<h1>About page 💻</h1>')
+  } else {
+    console.log('Request received')
+    res.statusCode = 404
+    res.end('<h1>Page not found 😕</h1>')
   }
 }
 
