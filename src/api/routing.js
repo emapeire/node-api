cb23111b380310f9088ee966bb72f5469b7d49de
Emanuel Peire <emapeire@outlook.com>
2023-12-07 00:02:39 -0300

    Add GET and POST endpoints for Pokemon API

--- src/api/routing.js
+++ src/api/routing.js
@@ -15,6 +15,27 @@ const processRequest = (req, res) => {
           res.setHeader('Content-Type', 'text/html; charset=utf-8')
           return res.end('<h1>Not Found 😕</h1>')
       }
+    case 'POST':
+      switch (url) {
+        case '/pokemon': {
+          let body = ''
+          req.on('data', (chunk) => {
+            body += chunk.toString()
+          })
+
+          req.on('end', () => {
+            const data = JSON.parse(body)
+            res.writeHead(201, { 'Content-Type': 'application/json' })
+            data.timestamp = new Date().toISOString()
+            res.end(JSON.stringify(data))
+          })
+          break
+        }
+        default:
+          res.statusCode = 404
+          res.setHeader('Content-Type', 'text/html; charset=utf-8')
+          return res.end('<h1>Not Found 😕</h1>')
+      }
   }
 }
 
